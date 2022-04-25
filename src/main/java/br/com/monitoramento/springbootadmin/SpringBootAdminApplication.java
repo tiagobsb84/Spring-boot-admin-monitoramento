bf1bd6768c1618ea916e727522436e06b36f4042
Tiago Silva <tiagobsb84@gmail.com>
2022-04-25 12:44:10 -0300

    Configurando o ambiente do monitoramento

--- src/main/java/br/com/monitoramento/springbootadmin/SpringBootAdminApplication.java
+++ src/main/java/br/com/monitoramento/springbootadmin/SpringBootAdminApplication.java
@@ -1,9 +1,14 @@
 package br.com.monitoramento.springbootadmin;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 public class SpringBootAdminApplication {
 
 	public static void main(String[] args) {
