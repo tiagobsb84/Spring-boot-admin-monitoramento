a9b5684dd5af000d11d7c8f5fb6ad67a8974c526
Tiago Silva <tiagobsb84@gmail.com>
2022-04-25 11:06:38 -0300

    Criando projeto para usar a interface de monitoramento do spring boot admin

---
+++ src/main/java/br/com/monitoramento/springbootadmin/SpringBootAdminApplication.java
@@ -0,0 +1,13 @@
+package br.com.monitoramento.springbootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+
+}
